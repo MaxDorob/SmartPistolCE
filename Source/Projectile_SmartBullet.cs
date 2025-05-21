6307bc15770314d9c4c796dc0bb86a6fdc3d6c9b
MaxDorob <maxdor81@gmail.com>
2025-05-22 02:55:33 +0600

    Early intercept (not sure if needed tho)

--- Source/Projectile_SmartBullet.cs
+++ Source/Projectile_SmartBullet.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace SmartPistol
 {
     public class Projectile_SmartBullet : BulletCE
     {
         public float targetHeight = 0f;
+        protected override bool CanCollideWith(Thing thing, out float dist)
+        {
+            if (thing == intendedTargetThing && (thing.DrawPos - ExactPosition).MagnitudeHorizontalSquared() < 1.2f)
+            {
+                dist = 0f;
+                return true;
+            }
+            return base.CanCollideWith(thing, out dist);
+        }
     }
 }
